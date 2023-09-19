2a211b312a35b0a1dca3e8e905631b7c932b5d8e
Ryan Dens <ryan.dens@pixee.ai>
2023-09-19 14:24:58 -0700

    :bug: fix vuln

--- src/main/java/hawk/service/UserSearchService.java
+++ src/main/java/hawk/service/UserSearchService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.EntityManager;
 import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -26,22 +27,12 @@ public class UserSearchService {
         final Session session = entityManager.unwrap(Session.class);
         return session.doReturningWork(connection -> {
             List<User> users1 = new ArrayList<>();
-            // The wrong way
-            String query = "select id, name, description, tenant_id from public.user where name like '%" +
-                    search.getSearchText() + "%'";
 
-            LOGGER.log(Level.INFO, "SQL Query {0}",  query);
-            ResultSet rs = connection
-                    .createStatement()
-                    .executeQuery(query);
-
-            /* The righter way, should probably use built in Data Model for this, but this is safe
             String query = "select id, name, description from ITEM where description like ?";
             PreparedStatement statement = connection.prepareStatement(query);
             statement.setString(1, "%" + search.getSearchText() + "%");
             LOGGER.log(Level.INFO, "SQL Query {0}",  statement);
             ResultSet rs = statement.executeQuery();
-            */
 
             while (rs.next()) {
                 users1.add(new User(rs.getLong("id"), rs.getString("name"), rs.getString("description"), rs.getString("tenant_id")));
